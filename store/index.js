cbdc4a07a1404539792f45160c43c182b9f8c033
StefanBehring <chadhz@gmx.de>
2021-12-10 16:58:56 +0100

    Edit functions to be async

--- store/index.js
+++ store/index.js
@@ -19,45 +19,47 @@ const createStore = () => {
       },
     },
     actions: {
-      nuxtServerInit(vuexContext, context) {
-        return axios
-          .get(
+      async nuxtServerInit(vuexContext, context) {
+        try {
+          const res = await axios.get(
             'https://nuxt-blog-5f7a5-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
           )
-          .then((res) => {
-            const postsArray = []
-            for (const key in res.data) {
-              postsArray.push({ ...res.data[key], id: key })
-            }
-            vuexContext.commit('setPosts', postsArray)
-          })
-          .catch((e) => context.error(e))
+          const postsArray = []
+          for (const key in res.data) {
+            postsArray.push({ ...res.data[key], id: key })
+          }
+          vuexContext.commit('setPosts', postsArray)
+        } catch (e) {
+          return context.error(e)
+        }
       },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPosts', posts)
       },
-      addPost(vuexContext, newPost) {
+      async addPost(vuexContext, newPost) {
         const createdPost = { ...newPost, updatedDate: new Date() }
-        return axios
-          .post(
+        try {
+          const res = await axios.post(
             'https://nuxt-blog-5f7a5-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
             createdPost
           )
-          .then((res) => {
-            vuexContext.commit('addPost', { ...createdPost, id: res.data.name })
-          })
-          .catch((e) => console.log(e))
+          vuexContext.commit('addPost', { ...createdPost, id: res.data.name })
+        } catch (e) {
+          return console.log(e)
+        }
       },
-      editPost(vuexContext, editedPost) {
-        return axios
-          .put(
+      async editPost(vuexContext, editedPost) {
+        try {
+          await axios.put(
             'https://nuxt-blog-5f7a5-default-rtdb.europe-west1.firebasedatabase.app/posts/' +
               editedPost.id +
               '.json',
             editedPost
           )
-          .then((res) => vuexContext.commit('editPost', editedPost))
-          .catch((e) => console.log(e))
+          return vuexContext.commit('editPost', editedPost)
+        } catch (e) {
+          return console.log(e)
+        }
       },
     },
     getters: {
